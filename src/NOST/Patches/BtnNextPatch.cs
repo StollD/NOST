ea4495cc628e98699d42473c8599166687ca49e8
Dorian Stoll <dorian.stoll@tmsp.io>
2018-11-30 16:19:09 +0100

    Replace the message box popups with actual OST errors

---
+++ src/NOST/Patches/BtnNextPatch.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Harmony;
+using MainForms;
+
+namespace NOST.Patches
+{
+    [HarmonyPatch(typeof(Form1))]
+    [HarmonyPatch("btnNext_Click")]
+    public class BtnPhoneKeyInfoPatch
+    {
+        public static Boolean Prefix(ref Form1 __instance)
+        {
+            PropertyInfo deviceConnected =
+                typeof(Form1).GetProperty("DeviceConnected", BindingFlags.Instance | BindingFlags.NonPublic);
+            if ((Boolean) deviceConnected.GetValue(__instance, null))
+            {
+                // Try to make the "Edit Phone Information" button actually useable, 
+                // as a display for fastboot oem device-info
+                Process p = new Process();
+                p.StartInfo.FileName = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location),
+                    "fastboot-android.exe");
+                p.StartInfo.Arguments = "oem device-info";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.Start();
+                p.WaitForExit();
+                String output = p.StandardError.ReadToEnd();
+
+                // Check if the bootloader is unlocked to critical
+                if (output.Contains("Device critical unlocked: false"))
+                {
+                    MethodInfo showErrorMessage = typeof(Form1)
+                        .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                        .FirstOrDefault(m =>
+                            m.Name == "ShowErrorMessage" && m.GetParameters().Select(pa => pa.ParameterType)
+                                .Contains(typeof(Int64)));
+                    showErrorMessage.Invoke(__instance,
+                        new Object[] {50723, null});
+
+                    // Disable the next button, it does nothing
+                    FieldInfo btnNext =
+                        typeof(Form1).GetField("btnNext", BindingFlags.Instance | BindingFlags.NonPublic);
+                    Button btn = (Button) btnNext.GetValue(__instance);
+                    btn.Enabled = false;
+                    btnNext.SetValue(__instance, btn);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- src/NOST/Patches/LoadFwImagePatch.cs
+++ src/NOST/Patches/LoadFwImagePatch.cs
@@ -2,8 +2,10 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 using Harmony;
+using MainForms;
 using NOST.Utilities;
 
 namespace NOST.Patches
@@ -12,7 +14,7 @@ namespace NOST.Patches
     [HarmonyPatch("LoadFwImage")]
     public class LoadFwImagePatch
     {
-        public static Boolean Prefix(ref String fwPath)
+        public static Boolean Prefix(ref String fwPath, ref Int64 __result)
         {
             // Check if the Image file is a .zip or .qlz file
             if (fwPath.EndsWith(".zip"))
@@ -20,47 +22,36 @@ namespace NOST.Patches
                 // TODO: Add .zip file support
                 return false;
             }
+
             if (fwPath.EndsWith(".qlz"))
             {
                 String directory = Path.GetTempFileName().Replace(".tmp", "");
-                
+
                 // Extract the file using QuickLZ
                 ProcessStartInfo info = new ProcessStartInfo();
-                info.FileName = Path.Combine(Path.Combine(Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), ".."), "Tools"), "exdupe.exe");
+                info.FileName =
+                    Path.Combine(
+                        Path.Combine(Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), ".."),
+                            "Tools"), "exdupe.exe");
                 info.Arguments = "-R " + fwPath + " " + directory;
-                
-                // Ask for confirmation
-                DialogResult result = MessageBox.Show(
-                    "Extracting the firmware image. This could take some time and the program might look like it froze. Please don't terminate it.",
-                    "Firmware Unpacker", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-                if (result == DialogResult.Cancel)
-                {
-                    return false;
-                }
-                
+
                 // Extract the file
                 Process decompressor = Process.Start(info);
                 if (decompressor != null)
                 {
                     decompressor.WaitForExit();
                 }
-                
+
                 // Try to get the extracted .mlf file
                 String mlfPath = Directory.GetFiles(directory, "*.mlf").FirstOrDefault();
                 if (String.IsNullOrEmpty(mlfPath))
                 {
-                    MessageBox.Show("Extracting the firmware failed, or it is invalid!", "Error", MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
+                    __result = 50718;
                     return false;
                 }
-                
+
                 // Update the selected firmware path
                 fwPath = mlfPath;
-                
-                // Announce success
-                MessageBox.Show("Successfully extracted the firmware!", "Success", MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-                return true;
             }
 
             return true;
